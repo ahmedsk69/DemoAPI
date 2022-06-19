4268dde9d1e7770ae2d194d611d6570f8495b59d
ahmedsk69 <ahmed.shaik78699@gmail.com>
2022-06-19 13:33:27 +0530

    configurable port

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.WebHost.UseUrls("http://localhost:5500/");
+//builder.WebHost.UseUrls("http://localhost:5500/");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
